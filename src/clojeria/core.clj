da76ad220d2b7291b68b26b6a9c5a3f7f53a851c
Doug Davis <ddavis@ddavis.io>
2020-08-11 19:38:32 -0400

    ready for tomorrow

--- src/clojeria/core.clj
+++ src/clojeria/core.clj
@@ -4,7 +4,7 @@
 
 (def history (atom []))
 
-(swap! history conj (g/init-from-csv "resources/2020.08.09.csv"))
+(swap! history conj (g/init-from-csv "resources/2020.08.11.csv"))
 
 (defn latest []
   (last @history))
@@ -80,10 +80,8 @@
 (zero-cards)
 
 (ch-cards "Guelita" 4)
-(ch-cards "Guelito" 4)
 (ch-cards "Blanca" 4)
 (ch-cards "Junior" 4)
 (ch-cards "Anna" 4)
-(ch-cards "Alfred" 4)
-(ch-cards "Kristie" 2)
+(ch-cards "Stephanie" 4)
 (ch-cards "Naidu" 4)
